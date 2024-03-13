3a46e1153fdadbf5ce438045d017e131f03fd6bd
Becca Rothwell <becca.rothwell@gmail.com>
2024-03-13 19:52:53 +0000

    feat: Initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '52ffd472d5b0b4eb6cbb');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e5eaa3c8e3502007484a');
