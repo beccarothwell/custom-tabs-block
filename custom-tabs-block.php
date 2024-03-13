709fbad77e1a2ef46ecec928db62686cc841d0e1
Becca Rothwell <becca.rothwell@gmail.com>
2024-03-13 20:37:51 +0000

    chore: Rename plugin and update text domain, change block namespace

--- custom-tabs-block.php
+++ custom-tabs-block.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name:       Tabs Block
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       Custom Tabs Block
+ * Description:       Custom Tabs Block scaffolded with Create Block tool.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Becca Rothwell
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       tabs-block
+ * Text Domain:       custom-tabs-block
  *
  * @package           create-block
  */
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function tabs_block_tabs_block_block_init() {
+function becca_rothwell_tabs_block_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
-add_action( 'init', 'tabs_block_tabs_block_block_init' );
+add_action( 'init', 'becca_rothwell_tabs_block_block_init' );
